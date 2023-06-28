5bb08ba15339e7d277aea3c5f81636947dce0ff9
Aziz LILOUDINI <zizoa1er@gmail.com>
2023-06-28 10:11:03 +0200

    Refacto FizzBuzz

--- src/main/java/fr/zizoa/apprendre/FizzBuzz.java
+++ src/main/java/fr/zizoa/apprendre/FizzBuzz.java
@@ -3,17 +3,16 @@ package fr.zizoa.apprendre;
 public class FizzBuzz {
 
     public static String printCorrespond(int i) {
-        var response = "";
-        if (isDivisibleBy(i, 3)) {
-            response += "Fizz";
-        }
-        if (isDivisibleBy(i, 5)) {
-            response += "Buzz";
-        }
+        /*
+          Cette version un peu compact peu ne pas être apprécié par certains, ça peut se
+          car ça peut paraître un peu difficilement maintenable que si on restait sur des if .. else
+         */
+        var response = isDivisibleBy(i, 3) ? "Fizz" : "";
+        response = isDivisibleBy(i, 5) ? response + "Buzz" : response;
         return response.equals("") ? String.valueOf(i) : response;
     }
 
-    private static boolean isDivisibleBy(int dividend, int divisor) {
-        return dividend % divisor == 0;
+    private static boolean isDivisibleBy(int numerateur, int denominateur) {
+        return numerateur % denominateur == 0;
     }
 }
--- src/test/java/fr/zizoa/apprendre/FizzBuzzTest.java
+++ src/test/java/fr/zizoa/apprendre/FizzBuzzTest.java
@@ -6,28 +6,28 @@ import org.junit.jupiter.api.Test;
 public class FizzBuzzTest {
 
     @Test
-    void shouldReturnNumberAsString() {
+    void shouldPrintNumberAsString() {
         Assertions.assertEquals(FizzBuzz.printCorrespond(2), "2");
         Assertions.assertEquals(FizzBuzz.printCorrespond(-2), "-2");
         Assertions.assertEquals(FizzBuzz.printCorrespond(17), "17");
     }
 
     @Test
-    void shouldReturnNumberAsFizz() {
+    void shouldPrintNumberAsFizz() {
         Assertions.assertEquals(FizzBuzz.printCorrespond(3), "Fizz");
         Assertions.assertEquals(FizzBuzz.printCorrespond(9), "Fizz");
         Assertions.assertEquals(FizzBuzz.printCorrespond(-9), "Fizz");
     }
 
     @Test
-    void shouldReturnNumberAsBuzz() {
+    void shouldPrintNumberAsBuzz() {
         Assertions.assertEquals(FizzBuzz.printCorrespond(5), "Buzz");
         Assertions.assertEquals(FizzBuzz.printCorrespond(10), "Buzz");
         Assertions.assertEquals(FizzBuzz.printCorrespond(-10), "Buzz");
     }
 
     @Test
-    void shouldReturnNumberAsFizzBuzz() {
+    void shouldPrintNumberAsFizzBuzz() {
         Assertions.assertEquals(FizzBuzz.printCorrespond(0), "FizzBuzz");
         Assertions.assertEquals(FizzBuzz.printCorrespond(15), "FizzBuzz");
         Assertions.assertEquals(FizzBuzz.printCorrespond(-15), "FizzBuzz");
